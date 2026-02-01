7fafe22eec3a7ede77481d4b6daac2224e51c476
youtheyeon <yuseoyeon@ewhain.net>
2026-02-02 00:12:40 +0900

    setting: Tailwind v4 기반 패키지 세팅

--- frontend/apps/client/src/app/page.tsx
+++ frontend/apps/client/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Client</div>;
+  return (
+    <main className='p-6 space-y-4'>
+      <div className='bg-brand text-white p-4'>TEST</div>
+    </main>
+  );
 }
