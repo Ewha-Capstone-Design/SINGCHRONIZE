bf268fb524546e5c0fa301e9d06e89deca9feccd
youtheyeon <yuseoyeon@ewhain.net>
2026-02-08 12:48:00 +0900

    feat: Tooltip Provider 설정 및 위치 조정

--- frontend/apps/client/src/app/layout.tsx
+++ frontend/apps/client/src/app/layout.tsx
@@ -1,9 +1,12 @@
 import './global.css';
+import { TooltipProvider } from '@singchronize/ui';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='ko'>
-      <body>{children}</body>
+      <body>
+        <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+      </body>
     </html>
   );
 };
