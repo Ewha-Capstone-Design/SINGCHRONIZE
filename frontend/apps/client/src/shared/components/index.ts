9d522cecc56e741b93c711f22015597fb85bb77d
youtheyeon <yuseoyeon@ewhain.net>
2026-02-24 19:37:01 +0900

    feat: Selectchip 구현

--- frontend/apps/client/src/shared/components/index.ts
+++ frontend/apps/client/src/shared/components/index.ts
@@ -1,4 +1,5 @@
 export { default as GenreCard } from './GenreCard';
+export { default as SelectChip } from './SelectChip';
 export { default as Sidebar } from './Sidebar';
 export { default as SituationCard } from './SituationCard';
 export { default as SongItem } from './SongItem';
