0e07b45cd9939569d6aa3cbc4ea573e51a441b4d
youtheyeon <yuseoyeon@ewhain.net>
2026-02-25 00:31:59 +0900

    style: 공통 컴포넌트 스타일 보완

--- frontend/packages/ui/src/components/index.ts
+++ frontend/packages/ui/src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './button/Button';
 export * from './dialog/Dialog';
 export * from './tooltip/Tooltip';
-export { default as Input } from './inputfield/InputField';
+export { default as InputField } from './inputfield/InputField';
