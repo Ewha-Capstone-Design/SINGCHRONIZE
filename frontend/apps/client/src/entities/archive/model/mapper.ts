a86de503999ed26caf878470b6022e557a5e90c0
youtheyeon <yuseoyeon@ewhain.net>
2026-03-03 23:17:07 +0900

    feat: 임시 데이터 추가

--- frontend/apps/client/src/entities/archive/model/mapper.ts
+++ frontend/apps/client/src/entities/archive/model/mapper.ts
@@ -1,12 +1,31 @@
 import { toSongUi } from '@/entities/song/model/mapper';
 import type { ArchiveSectionAPiType, ArchiveSectionUiType } from './types';
+import type { ArchiveItemApiType, ArchiveItemUiType } from './types';
+import type { ArchiveApiType, ArchiveUiType } from './types';
 import { getArchiveHistoryTitle } from './utils';
 
-export const toArchiveSectionUi = (
-  raw: ArchiveSectionAPiType
-): ArchiveSectionUiType => ({
+export const toArchiveSectionUi = (raw: ArchiveSectionAPiType): ArchiveSectionUiType => ({
   id: raw.rec_id,
   title: getArchiveHistoryTitle(raw.date),
   recordingUrl: raw.recording_url,
   songs: raw.songs.map(toSongUi),
 });
+
+export const toArchiveItemUi = (item: ArchiveItemApiType): ArchiveItemUiType => {
+  return {
+    id: item.id,
+    title: item.title,
+    artist: item.artist,
+    thumbnail: item.album_cover,
+    matchRate: item.match_rate,
+    isLiked: item.is_liked,
+  };
+};
+
+export const toArchiveUi = (data: ArchiveApiType): ArchiveUiType => {
+  return {
+    genres: data.genres,
+    situations: data.situations,
+    items: data.items.map(toArchiveItemUi),
+  };
+};
--- frontend/apps/client/src/entities/archive/model/types.ts
+++ frontend/apps/client/src/entities/archive/model/types.ts
@@ -1,4 +1,5 @@
 import type { SongApiType, SongUiType } from '@/entities/song/model/types';
+import { GenreKey, SituationKey } from '@/shared/types/category';
 
 export type ArchiveSectionAPiType = {
   rec_id: string;
@@ -12,4 +13,34 @@ export type ArchiveSectionUiType = {
   title: string;
   recordingUrl?: string;
   songs: SongUiType[];
+};
+
+export type ArchiveItemApiType = {
+  id: string;
+  title: string;
+  artist: string;
+  album_cover: string | null;
+  match_rate: number;
+  is_liked: boolean;
+};
+
+export type ArchiveItemUiType = {
+  id: string;
+  title: string;
+  artist: string;
+  thumbnail: string | null;
+  matchRate: number;
+  isLiked: boolean;
+};
+
+export type ArchiveApiType = {
+  genres: GenreKey[];
+  situations: SituationKey[];
+  items: ArchiveItemApiType[];
+};
+
+export type ArchiveUiType = {
+  genres: GenreKey[];
+  situations: SituationKey[];
+  items: ArchiveItemUiType[];
 };
