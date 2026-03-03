0c74a799253258a2d13c17adc537440d4723f67c
youtheyeon <yuseoyeon@ewhain.net>
2026-03-03 23:46:00 +0900

    feat: 홈 페이지 메인 배너 구현

--- frontend/apps/client/src/app/(main)/home/page.tsx
+++ frontend/apps/client/src/app/(main)/home/page.tsx
@@ -1,8 +1,27 @@
+import {
+  ArchivePreview,
+  HomeHeader,
+  VocalAnalysisBanner,
+  WeeklyChart,
+} from '@/widgets/home/ui';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <HomeHeader />
+
+      <div className='px-9 pb-9 grid grid-cols-[1fr_400px] gap-9'>
+        <div className='flex flex-col gap-8'>
+          <VocalAnalysisBanner />
+          {/* 라이브 기능 디자인 위치 */}
+        </div>
+
+        <div className='flex flex-col gap-12'>
+          <WeeklyChart />
+          <ArchivePreview />
+        </div>
+      </div>
+    </main>
   );
 };
 
