98f6ca88c3fda7b61795a222be087816d2dce059
Minju Kim <49716188+Minju-Kimm@users.noreply.github.com>
2026-03-12 16:51:27 +0900

    refactor: 외부 API 호출을 비동기 http 클라이언트로 전환

--- backend/app/routers/songs.py
+++ backend/app/routers/songs.py
@@ -8,15 +8,16 @@ spotify = SpotifyService(settings.SPOTIFY_CLIENT_ID, settings.SPOTIFY_CLIENT_SEC
 
 @router.get("/search")
 async def search_music(q: str):
-    results = spotify.search_tracks(q)
-    
+    results = await spotify.search_tracks(q)
+
     # 필요한 정보(제목, 가수, 앨범커버, URI)만 추려서 프론트에 전달
     tracks = []
     for item in results.get("tracks", {}).get("items", []):
+        images = item["album"].get("images", [])
         tracks.append({
             "name": item["name"],
             "artist": item["artists"][0]["name"],
-            "album_image": item["album"]["images"][0]["url"],
-            "uri": item["uri"] # 나중에 재생할 때 필수!
+            "album_image": images[0]["url"] if images else None,
+            "uri": item["uri"],
         })
     return tracks
