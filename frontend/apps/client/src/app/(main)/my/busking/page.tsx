762046704681934d1ed056f1fb7bd428b78b716b
youtheyeon <yuseoyeon@ewhain.net>
2026-03-13 19:17:30 +0900

    feat: 나의 온라인 버스킹 페이지 UI 구현

--- frontend/apps/client/src/app/(main)/my/busking/page.tsx
+++ frontend/apps/client/src/app/(main)/my/busking/page.tsx
@@ -1,7 +1,18 @@
+import { BuskingCard, PageHeader } from '@/shared/components';
+
+import { MOCK_BUSKING_LIST } from '@/entities/busking/model/mock';
+
 const MyBuskingPage = () => {
   return (
-    <div>
-      <h1>My Busking</h1>
+    <div className='py-13 flex flex-col gap-10'>
+      <PageHeader title='나의 온라인 버스킹' subText={`최근 업데이트 2026.02.03`} />
+      <div className='mx-auto w-fit'>
+        <div className='grid grid-cols-4 gap-x-4 gap-y-7'>
+          {MOCK_BUSKING_LIST.map((busking) => (
+            <BuskingCard key={busking.id} variant='sm' busking={busking} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
