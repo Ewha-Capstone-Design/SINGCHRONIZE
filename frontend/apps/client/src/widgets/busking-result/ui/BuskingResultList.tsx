1d4c54cc53d765036c4c6ef8978c4f1bf5aa6c48
youtheyeon <yuseoyeon@ewhain.net>
2026-03-23 20:49:27 +0900

    style: 라이브 결과 페이지 홈 이동 버튼 마진 수정

--- frontend/apps/client/src/widgets/busking-result/ui/BuskingResultList.tsx
+++ frontend/apps/client/src/widgets/busking-result/ui/BuskingResultList.tsx
@@ -10,7 +10,7 @@ type BuskingResultListProps = {
 
 const BuskingResultList = ({ streamerName, items }: BuskingResultListProps) => {
   return (
-    <div className='relative mx-auto px-12 py-16 flex w-full max-w-270 flex-col'>
+    <div className='relative mx-auto px-12 pt-16 flex w-full max-w-270 flex-col'>
       <h1 className='text-center typo-32b text-white'>
         {streamerName}님의 라이브 버스킹 결과를 살펴보세요!
       </h1>
