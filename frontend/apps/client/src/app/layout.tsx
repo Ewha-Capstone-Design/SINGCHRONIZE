4fac437d23cfe737c5df92fd41572651a6466276
youtheyeon <yuseoyeon@ewhain.net>
2026-03-29 22:03:42 +0900

    feat: API 연결 세팅

--- frontend/apps/client/src/app/layout.tsx
+++ frontend/apps/client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { metadataConfig } from './metadata';
 import './global.css';
+import { Providers } from './providers';
 import { TooltipProvider } from '@singchronize/ui';
 
 export const metadata: Metadata = metadataConfig;
@@ -9,7 +10,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='ko'>
       <body className='h-screen bg-bg scrollbar-hide'>
-        <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+        <Providers>
+          <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+        </Providers>
       </body>
     </html>
   );
